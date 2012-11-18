923790e7aa39ca5057f0f081b9a2a6b86897cda8
ypeels <ypeels@live.com>
2012-11-17 19:55:40 -0600

    next: ArticlesController? but testing is BUTT slow - next try to speed it up with spork?

---
+++ spec/controllers/my_articles_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe ArticlesController do
+
+  describe 'merging 2 articles' do
+  
+    # like movies_controller_spec.rb
+    it 'should call the model method that performs article merging' do
+      pending
+    end
+  
+  end
+end
